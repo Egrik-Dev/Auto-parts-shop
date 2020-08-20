56e15fac9ab450130e6d4717522f7ffc69e0e81d
Egrik <egor-office-mail@yandex.ru>
2020-08-20 23:42:27 +0300

    Сделал исправления в работе боковой навигации и фильтров

--- source/js/hamburger-nav.js
+++ source/js/hamburger-nav.js
@@ -1,22 +1,19 @@
-const SideNavStatus = Object.create(null);
-SideNavStatus.OPENED = `opened`;
-SideNavStatus.CLOSED = `closed`;
-
 const hamburgerBtn = document.querySelector(`.user-menu__nav-toggle`);
 const sideMenu = document.querySelector(`.main-wrapper__hamburger-nav`);
 const mainContainer = document.querySelector(`.main-wrapper__content-container`);
+const overlayElement = document.querySelector(`.overlay`);
 const bodyElement = document.querySelector(`body`);
 
 hamburgerBtn.addEventListener(`click`, () => {
-  if (hamburgerBtn.dataset.navStatus === SideNavStatus.CLOSED) {
-    hamburgerBtn.dataset.navStatus = SideNavStatus.OPENED;
-    sideMenu.classList.add(`main-wrapper__hamburger-nav--open`);
-    mainContainer.classList.add(`main-wrapper__content-container--filter-active`);
-    bodyElement.style.overflow = `hidden`;
-  } else {
-    hamburgerBtn.dataset.navStatus = SideNavStatus.CLOSED;
-    sideMenu.classList.remove(`main-wrapper__hamburger-nav--open`);
-    mainContainer.classList.remove(`main-wrapper__content-container--filter-active`);
-    bodyElement.removeAttribute(`style`);
-  }
+  sideMenu.classList.add(`main-wrapper__hamburger-nav--open`);
+  mainContainer.classList.add(`main-wrapper__content-container--filter-active`);
+  overlayElement.classList.add(`overlay--show`);
+  bodyElement.style.overflow = `hidden`;
+});
+
+overlayElement.addEventListener(`click`, () => {
+  sideMenu.classList.remove(`main-wrapper__hamburger-nav--open`);
+  mainContainer.classList.remove(`main-wrapper__content-container--filter-active`);
+  overlayElement.classList.remove(`overlay--show`);
+  bodyElement.removeAttribute(`style`);
 });
