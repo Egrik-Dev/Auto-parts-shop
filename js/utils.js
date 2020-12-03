b79d2fbf7b2df3afc0e2b21e77cff3bb1a59c43c
Egrik-Dev <Egrik-Dev@users.noreply.github.com>
2020-12-03 20:19:05 +0000

    deploy: 88b840e1089206753f2c6ee8f2055d8436da941e

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-// Создаём контейнер для корректной вставки в HTML
-export const createElement = (template) => {
-  const newElement = document.createElement(`div`);
-  newElement.innerHTML = template;
-
-  return newElement.firstChild;
-};
