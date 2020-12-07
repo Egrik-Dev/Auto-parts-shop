a4b7079b6fed3d05c5413f11e4c6c1de44e3b23b
Egrik <egor-office-mail@yandex.ru>
2020-12-07 06:31:20 +0300

    Фиксит работа выезжающее боковое меню в каталоге

--- source/js/hamburger-nav.js
+++ source/js/hamburger-nav.js
@@ -1,7 +1,7 @@
 const hamburgerBtn = document.querySelector(`.user-menu__nav-toggle`);
 const sideMenu = document.querySelector(`.main-wrapper__hamburger-nav`);
 const mainContainer = document.querySelector(`.main-wrapper__content-container`);
-const overlayElement = document.querySelector(`.overlay`);
+const overlayElement = document.querySelector(`[data-block="page-overlay"]`);
 const bodyElement = document.querySelector(`body`);
 
 hamburgerBtn.addEventListener(`click`, () => {
